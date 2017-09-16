a273164a366d8c6be8a093ebb628bf80eaaa8de6
Dragos <Dragos@DESKTOP-3ITVK1F>
2017-09-16 19:20:32 +0300

    ...

---
+++ Java6DamianDragos/src/calculator/Screen.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Dragos
+ */
+public class Screen {
+    String screenMemory;
+    
+    public void display(String ){
+    
+    
+    
+    
+    
+    }
+}
