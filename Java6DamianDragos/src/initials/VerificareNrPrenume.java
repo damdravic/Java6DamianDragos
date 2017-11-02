6035368885901b49ef6f6b960fcecce24084ab1d
Dragos <Dragos@DESKTOP-3ITVK1F>
2017-11-02 22:09:07 +0200

    ...

---
+++ Java6DamianDragos/src/initials/VerificareNrPrenume.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package initials;
+import java.util.Scanner;
+/**
+ *
+ * @author Dragos
+ */
+public class VerificareNrPrenume {
+    Scanner scan =  new Scanner(System.in);
+    private int nrp;
+    int[] posibility={1,2};
+    
+    public int getNrp(){
+        return nrp;
+    }
+    
+
+    
+    public void setNrp(){ 
+        
+        System.out.println("Cate prenume aveti?");
+        String input;
+        input = scan.nextLine();
+        try{
+        this.nrp = Integer.parseInt(input);
+        }
+        catch(NumberFormatException e){
+             System.out.println("Incearca un numar intreg.");
+             setNrp();
+        }
+        
+        
+        
+      
+        
+        
+        
+          
+            
+       
+   
+    }
+    
+}
